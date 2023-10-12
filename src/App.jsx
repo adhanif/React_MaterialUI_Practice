e517d38588d0f57e65b7989ec0594fa13e6feea5
ad.hanif <ad.hanif@outlook.com>
2023-10-12 11:41:17 +0200

    button created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Button, ButtonGroup } from "@mui/material";
+import { Save, Delete } from "@mui/icons-material";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ButtonGroup>
+        <Button
+          size="large"
+          startIcon={<Save />}
+          style={{ fontSize: "20px" }}
+          variant="contained"
+          color="primary"
+        >
+          Save
+        </Button>
+        <Button
+          size="large"
+          startIcon={<Delete />}
+          style={{ fontSize: "20px" }}
+          variant="contained"
+          color="secondary"
+        >
+          Discard
+        </Button>
+      </ButtonGroup>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
